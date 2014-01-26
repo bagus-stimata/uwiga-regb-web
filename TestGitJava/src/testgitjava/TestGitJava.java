ab902d14afd24328adf3d77226d5e5295a649230
yhawin <yhawin@yhawin-ubuntu-13.04>
2014-01-26 10:17:42 +0700

    penanda pertama ok!!

---
+++ TestGitJava/src/testgitjava/TestGitJava.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testgitjava;
+
+/**
+ *
+ * @author yhawin
+ */
+public class TestGitJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Tulisann Yang pertama");
+    }
+    
+}
